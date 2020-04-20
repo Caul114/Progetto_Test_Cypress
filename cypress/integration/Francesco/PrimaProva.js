e0d48fad083a83ad18289c85ed673052571d003e
Francesco Zanardi <zanardifrancesco98@gmail.com>
2020-04-20 10:30:44 +0200

    risoluzione primo errore

--- cypress/integration/Francesco/PrimaProva.js
+++ cypress/integration/Francesco/PrimaProva.js
@@ -1,14 +1,11 @@
-describe("HomePage", function () {
-  it("Visit HomePage", function () {
-    // cy.visit("localhost:3005/"); // url da visitare
-    // cy.url().should("include", "localhost:3005");
-    // cy.visit("https://www.shop-o-rama.it/"); // url da visitare
-    // cy.url().should("include", "www.shop-o-rama.it");
-    cy.visit("https://sor-fe-staging.herokuapp.com/"); // url da visitare
-    cy.url().should("include", "sor-fe-staging.herokuapp.com/");
-    
-    // da molti più errori se si utilizza il codice localhost
-    //cy.visit("http://localhost:3005/"); // url da visitare
-    //cy.url().should("include", "localhost:3005");
+describe("Products section - Our bestsellers", function () {
+  beforeEach(() => {
+    cy.visit("http://localhost:3005/");
+  });
+
+
+  it("Logo Shop-o-rama", function () {
+    cy.get('[data-test="LogoLink_Header"]').click({ force: true }); // Logo di Shop-o-rama
+    cy.url().should("include", "localhost:3005");
   });
 });
